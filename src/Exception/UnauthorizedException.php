74abfc1b9cb71294f801b2692c155323060ce22b
frank <frank@qlimix.net>
2019-02-16 19:11:23 +0100

    pass exception downstream

--- src/Exception/UnauthorizedException.php
+++ src/Exception/UnauthorizedException.php
@@ -2,6 +2,12 @@
 
 namespace Qlimix\Action\Exception;
 
+use Throwable;
+
 final class UnauthorizedException extends ActionException
 {
+    public function __construct(Throwable $exception)
+    {
+        parent::__construct('', 0, $exception);
+    }
 }
